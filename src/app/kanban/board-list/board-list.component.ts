510b4780c53b7a83bbaa56213ac6b40514c4eb82
Sam <programming@vectors.net.au>
2019-10-20 21:33:36 +0800

    board setup

---
+++ src/app/kanban/board-list/board-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Board } from '../board.model';
+import { Subscription } from 'rxjs';
+import { BoardService } from '../board.service';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-board-list',
+  templateUrl: './board-list.component.html',
+  styleUrls: ['./board-list.component.scss']
+})
+export class BoardListComponent implements OnInit, OnDestroy {
+  boards: Board[];
+  sub: Subscription;
+
+  constructor(public boardService: BoardService) { }
+
+  ngOnInit() {
+    this.sub = this.boardService
+      .getUserBoards()
+      .subscribe(boards => (this.boards = boards));
+  }
+
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
+    this.boardService.sortBoards(this.boards);
+  }
+
+}
---
+++ src/app/kanban/board/board.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { BoardService } from '../board.service';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent {
+  @Input() board;
+
+  constructor(private boardService: BoardService) { }
+
+  taskDrop(event: CdkDragDrop<string[]>){
+    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
+    this.boardService.updateTasks(this.board.id, this.board.tasks);
+  }
+
+}
